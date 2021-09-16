e3403fc45ada3aae992ab50793eadbada2d347f4
RachaelJeikishore <90875629+RachaelJeikishore@users.noreply.github.com>
2021-09-17 09:28:35 +1000

    Initial commit

---
+++ app/src/main/java/com/example/cofeeapp01/Order.java
@@ -0,0 +1,53 @@
+package com.example.cofeeapp01;
+
+public class Order {
+    // attributes of every Order
+    private int _id;
+    private String _custName;
+    private int _saleAmount;
+
+
+    // default constructor
+
+    public Order() {
+        _id=0;
+        _custName=null;
+        _saleAmount=0;
+    }
+
+
+    // 2nd constructor
+
+    public Order(String _custName, int _saleAmount) {
+        this._custName = _custName;
+        this._saleAmount = _saleAmount;
+    }
+
+//set
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public void set_custName(String _custName) {
+        this._custName = _custName;
+    }
+
+    public void set_saleAmount(int _saleAmount) {
+        this._saleAmount = _saleAmount;
+    }
+
+    // get
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String get_custName() {
+        return _custName;
+    }
+
+    public int get_saleAmount() {
+        return _saleAmount;
+    }
+}
